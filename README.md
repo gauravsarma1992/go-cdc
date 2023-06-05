f071083b1eb64cc3c945a2b99e42396bf52979fb
Gaurav Sarma <gauravsarma1992@gmail.com>
2023-06-05 19:27:14 +0530

    Initialising StageTracker

--- README.md
+++ README.md
@@ -1,6 +1,16 @@
 # Mongoreplay 
 CDC for MongoDB written in Golang
 
+## Flow of copying data across collections
+- Fetch the list of collections to be copied
+- Revert if the collection is already created in the new DB
+- Create new collection with the required indexes
+- Note the current timestamp which can be used for the oplog tailing
+- Start dumping the existing documents from the new collection
+- Once the dumping is complete, start the oplog tailing
+- Once the timestamp is nearby the current timestamp, the oplog tailing
+can be shutdown
+
 ## Config files
 All the config files are present in `src/oplog/config` folder
 ```
@@ -15,7 +25,6 @@ make run # Runs the mongo container and the mongoreplay container
 make test # Runs the test suite
 make build # Builds the app 
 ```
-
 
 ## TODO (sorted based on descending priority)
 - Proper shutdown of the concurrent contexts
---
+++ oplog/stage_tracker.go
@@ -0,0 +1,49 @@
+package oplog
+
+import (
+	"context"
+	"time"
+)
+
+type (
+	StageTypeT uint8
+	StateTypeT uint8
+
+	StageTracker struct {
+		Ctx    context.Context
+		Stages []Stage
+	}
+	Stage struct {
+		StartTime       time.Time `json:"start_time"`
+		StopTime        time.Time `json:"stop_time"`
+		LastHeartbeatAt time.Time `json:"last_heartbeat_at"`
+
+		StageType StageTypeT `json:"stage_type"`
+		Status    StateTypeT `json:"status"`
+
+		Metadata map[string]interface{} `json:"metadata"`
+	}
+)
+
+var (
+	// Stages
+	InitStage                StageTypeT = 0
+	PreparingCollectionStage StageTypeT = 1
+	DumpingCollectionStage   StageTypeT = 2
+	TailingOplogStage        StageTypeT = 3
+
+	// States
+	SuccessState StateTypeT = 0
+	FailedState  StateTypeT = 1
+)
+
+func NewStageTracker(ctx context.Context) (stageTracker *StageTracker, err error) {
+	stageTracker = &StageTracker{
+		Ctx: ctx,
+	}
+	return
+}
+
+func (stageTracker *StageTracker) Track() (err error) {
+	return
+}
